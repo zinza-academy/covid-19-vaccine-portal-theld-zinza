b369df4fd9fb241606b4c39c6a0026817916fbff
Theld <trieu.nx@zinza.com.vn>
2023-12-13 11:30:14 +0700

    implement login, add yup package

---
+++ src/layouts/AuthLayout.tsx
@@ -0,0 +1,19 @@
+import authImage from '../assets/img/auth1.png';
+import { Outlet } from 'react-router-dom';
+
+const AuthLayout: React.FC = () => {
+  return (
+    <div>
+      <div className="flex h-screen">
+        <div
+          className="hidden lg:block lg:w-1/2 bg-cover bg-center"
+          style={{ backgroundImage: `url(${authImage})` }}></div>
+        <div className="w-full h-full flex items-center justify-center lg:w-1/2 bg-white px-10 py-16 overflow-y-auto">
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AuthLayout;
